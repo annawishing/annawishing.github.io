6f71073586c6dbfe3068f68417a059de534732c9
annawishing <annawishing@gmail.com>
2016-01-13 16:04:22 +0800

    add a new page named traveling, using bootstrap

--- change-background/change-background.js
+++ change-background/change-background.js
@@ -1,7 +1,16 @@
+// $(document).ready(function(){
+
+// 	$("#button").on("click",function(){
+// 		var a = "#" + Math.random().toString(16).slice(2,8);
+// 		$("body").css({background:a})
+// 	})
+// })
+
+
 $(document).ready(function(){
 
-	$("#button").on("click",function(){
-		var a = "#" + Math.random().toString(16).slice(2,8)
-		$("body").css({background:a})
+	document.getElementById("button").addEventListener("click", function(){
+		var color = "#" + Math.random().toString(16).slice(2,8);
+		document.body.style.background = color;
 	})
 })
--- pick-color/pick-color.js
+++ pick-color/pick-color.js
@@ -1,13 +1,37 @@
+// $(document).ready(function(){
+
+// 	var a = "";
+
+// 	$(".color").on("click",function(){
+// 		a = $(this).css("background-color");
+// 	})
+
+// 	$(".box").on("click", function(){
+// 		$(this).css("background-color",a);
+// 	})
+
+// })
+
+// 重要知识点，困扰之处：如何获取元素真正的样式，这里使用window.getComputedStyle(element)解决。注意：element.style.xxx 取到的是内联的样式。
 $(document).ready(function(){
-
-	var a = "";
-
-	$(".color").on("click",function(){
-		a = $(this).css("background-color");
-	})
-
-	$(".box").on("click", function(){
-		$(this).css("background-color",a);
-	})
-
+	var color;
+	var colors = document.querySelectorAll(".color");
+	for (var i = 0; i < colors.length; i++) {
+		colors[i].addEventListener("click",function(){
+			color = window.getComputedStyle(this).backgroundColor;
+		})
+	};
+		
+	var boxs = document.querySelectorAll(".box");
+	for (var l = 0; l < boxs.length; l++) {
+		boxs[l].addEventListener("click",function(){
+			this.style.backgroundColor = color;
+		})
+	};
+	
 })
+
+
+// function getConstructorName(obj){
+// 	return obj && obj.constructor && obj.constructor.toString().match(/function\s*([^(]*)/)[1];
+// }
